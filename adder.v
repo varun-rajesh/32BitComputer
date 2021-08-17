7a7a23f39ae27fdbe360546c76110d11e01c1fa6
Varun Rajesh <varun.rajesh81@gmail.com>
2021-08-16 21:48:15 -0400

    Update adder.v
    Removed single bit adder in favor of quad bit look ahead adder

--- adder.v
+++ adder.v
@@ -1,40 +1,34 @@
 //32 bit adder
-module add(input[31:0] a, b, output[31:0] c);
-  wire[31:0] c_out;
+module add(input[31:0] a, b, output[31:0] c, output overflow);
+  wire[7:0] c_out;
 
   //Size of adder
-  parameter N=32;
+  parameter N=32 / 4;
   genvar i;
+
+  assign overflow = c_out[7];
 
   generate
     for(i = 0; i < N; i = i + 1) begin
       if(i == 0) begin
-        //Half adder
-        sb_add sba(
-          .a (a[0]),
-          .b (b[0]),
+        qb_add qba(
+          .a (a[3 : 0]),
+          .b (b[3 : 0]),
           .c_in (1'b0),
-          .c (c[0]),
+          .c (c[3 : 0]),
           .c_out (c_out[0])
         );
       end else begin
-        //Full adder
-        sb_add sba(
-          .a (a[i]),
-          .b (b[i]),
+        qb_add qba(
+          .a (a[4 * i + 3 : 4 * i]),
+          .b (b[4 * i + 3 : 4 * i]),
           .c_in (c_out[i - 1]),
-          .c (c[i]),
+          .c (c[4 * i + 3 : 4 * i]),
           .c_out (c_out[i])
         );
       end
     end
   endgenerate
-endmodule
-
-//Single bit adder
-module sb_add(input a, b, c_in, output reg c, c_out);
-  always @ * c <= #14 c_in ^ (a ^ b);
-  always @ * c_out <= #24 (c_in & (a ^ b)) | (a & b);
 endmodule
 
 //4 bit carry look ahead adder
