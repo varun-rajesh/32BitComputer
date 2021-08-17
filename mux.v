a52647c61cbd7be3e021593ec2ae8eb58ca1c149
Varun Rajesh <varun.rajesh81@gmail.com>
2021-08-17 17:54:24 -0400

    Update mux.v
    added two word mux

--- mux.v
+++ mux.v
@@ -2,6 +2,15 @@ module two_mux(input a, b, sel, output out);
   assign out = (sel & a) | (~sel & b);
 endmodule
 
+module two_word_mux(input[63 : 0] a, b, input sel, input[63 : 0] out);
+  mux mux[63 : 0](
+    .a (a),
+    .b (b),
+    .sel (sel),
+    .out (out)
+  );
+endmodule
+
 module word_mux(input[31 : 0] a, b, input sel, input[31 : 0] out);
   mux mux[31 : 0](
     .a (a),
