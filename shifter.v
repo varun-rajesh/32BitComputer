609b453b86a9fa14cfa76bad559e604feaaed10d
Varun Rajesh <varun.rajesh81@gmail.com>
2021-08-17 16:19:16 -0400

    Update shifter.v
    added shift right by n bits unsigned

--- shifter.v
+++ shifter.v
@@ -48,3 +48,30 @@ module shift_right_signed(input[31 : 0] in, input[4 : 0] shift, output[31 : 0] o
     end
   endgenerate
 endmodule
+
+
+module shift_right_unsigned(input[31 : 0] in, input[4 : 0] shift, output[31 : 0] out);
+  integer i, j;
+  genvar k;
+  parameter N = 32;
+  reg[1023 : 0] choices;
+  reg[31 : 0] temp;
+
+  always @ * begin
+    for(i = 0; i < N; i = i + 1) begin
+      for(j = 0; j < N; j = j + 1) begin
+        choices[{i[4 : 0], j[4 : 0]}] <= (i + j < N) ? in[i + j] : 1'b0;
+      end
+    end
+  end
+
+  generate
+    for(k = 0; k < N; k = k + 1) begin
+      thirtytwo_mux mux(
+        .a (choices[32 * k + 31 : 32 * k]),
+        .sel (shift),
+        .out (out[k])
+      );
+    end
+  endgenerate
+endmodule
