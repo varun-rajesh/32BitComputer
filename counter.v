4b3c26d3ed84668eeac1b01b24ba919b8705ad3e
Varun Rajesh <varun.rajesh81@gmail.com>
2021-08-17 17:29:23 -0400

    Update counter.v
    updated counter for full 32 bit

--- counter.v
+++ counter.v
@@ -1,11 +1,11 @@
-module counter(input clk, reset, load, input[7:0] limit, load_val, output[7:0] count);
+module counter(input clk, reset, load, input[31:0] limit, load_val, output[31:0] count);
 
-  wire[7:0] n, m;
-  wire[7:0] count_bar;
-  wire[7:0] count_p;
+  wire[31:0] n, m;
+  wire[31:0] count_bar;
+  wire[31:0] count_p;
   wire carry;
 
-  d_ff dff[7:0](
+  d_ff dff[31:0](
     .clk (clk),
     .d (m),
     .preset (1'b1),
@@ -16,19 +16,19 @@ module counter(input clk, reset, load, input[7:0] limit, load_val, output[7:0] c
 
   add a(
     .a (count),
-    .b (8'b1),
+    .b (32'b1),
     .c (count_p),
     .carry (carry)
   );
 
-  byte_mux mux0(
-    .a (8'b00000000),
+  word_mux mux0(
+    .a (32'b0),
     .b (count_p),
     .sel (count_p == limit),
     .out (n)
   );
 
-  byte_mux mux1(
+  word_mux mux1(
     .a (load_val),
     .b (n),
     .sel (load),
