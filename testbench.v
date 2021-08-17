d0ba97efd5a7d71542685437c2c8b9bbf1a1ee65
Varun Rajesh <varun.rajesh81@gmail.com>
2021-08-16 22:47:47 -0400

    Update testbench.v
    testbench now tests for subtraction

--- testbench.v
+++ testbench.v
@@ -1,30 +1,33 @@
 `timescale 1 ns/1 ns
 `include "adder.v"
+`include "mux.v"
+`include "subtracter.v"
 
 module testbench;
 
   reg[31:0] a, b;
   wire[31:0] c;
+  wire overflow;
 
   initial begin
 
     $dumpfile("testbench.vcd");
     $dumpvars;
 
-    a = 32'h4a9a;
-    b = 32'h11e3;
+    b = 32'h1d834a9a;
+    a = 32'h23fc11e3;
 
-    #50
+    #5000
     a = 1;
     b = 0;
 
-    #50
+    #5000
     a = 0;
     b = 1;
 
     #50 $finish;
   end
 
-  add adder(a, b, c);
+  subtract sub(a, b, c, overflow);
 
 endmodule
