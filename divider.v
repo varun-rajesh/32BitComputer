327d6ff9845d157f4cef5d3fb1f12d05a33d19ef
Varun Rajesh <varun.rajesh81@gmail.com>
2021-08-19 10:07:07 -0400

    Update divider.v
    added remainder support to divider

--- divider.v
+++ divider.v
@@ -1,17 +1,19 @@
-module divide_unsigned(input clk, reset, input[31 : 0] a, b, output[31 : 0] out, output ready);
+module divide_unsigned(input clk, reset, input[31 : 0] a, b, output[31 : 0] quotient, remainder, output ready);
 
   wire[63 : 0] reg_in, reg_out, reg_out_shifted;
   wire[31 : 0] count, reg_out_subtracted, reg_out_added;
   wire overflow;
 
   assign reg_in = (count == 0) ? {31'b0, a, 1'b0} : {reg_out_shifted[63 : 1], !overflow};
-  assign out = {reg_out_shifted[31 : 1], !overflow};
+  assign quotient = {reg_out_shifted[31 : 1], !overflow};
+  assign remainder = reg_out_added;
+  assign ready = (count == 32'h20);
 
   counter counter(
     .clk (clk),
     .reset (reset),
     .load (1'b0),
-    .limit (32'h20),
+    .limit (32'h21),
     .load_val (),
     .count (count)
   );
